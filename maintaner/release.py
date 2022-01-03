0d8d1704c0c297f41cdfdacaf4a185469b050bb1
antonio <a.cavallo@cavallinux.eu>
2022-01-02 21:04:23 -0500

    add script

--- maintaner/release.py
+++ maintaner/release.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-from setuptools_github.start_release import parse_args, main
+from setuptools_github.start_release import main
 
 
 if __name__ == "__main__":
-    main(**parse_args())
+    main()
