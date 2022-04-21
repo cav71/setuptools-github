73229df9db82fe80cca181ddce7f63ca03dd8c96
antonio <a.cavallo@cavallinux.eu>
2022-04-21 07:18:15 -0400

    udpate

--- src/setuptools_github/start_release.py
+++ src/setuptools_github/start_release.py
@@ -1,5 +1,6 @@
 import pathlib
 import logging
+import re
 
 import pygit2  # type: ignore
 
@@ -23,31 +24,39 @@ def newversion(version, mode):
     return ".".join(str(v) for v in newver)
 
 
-def repo_checks(repo, error, dryrun, force, curver, newbranch):
+def extract_beta_branches(branches, remote=None):
+    result = set()
+    for branch in branches:
+        match = branch.partition("/")[0]
+        if remote and remote != match:
+            continue
+        if re.search(r"beta/\d+([.]\d+)*", branch):
+            result.add(branch)
+    return result
+
+
+def repo_checks(repo, remote, error, dryrun, force, curver, mode):
+
     # check repo has a single remote
     remotes = {remote.name for remote in repo.remotes}
-    if not remotes:
-        error("no remotes defined")
-    if len(remotes) > 1:
-        error(f"multiple remotes defined: {', '.join(remotes)}")
-    remote = remotes.pop()
-
-    # check the current version has a beta/<curver> branch
-    branche_names = set(
-        r.partition("/")[2] if r.partition("/")[0] == remote else r
-        for r in repo.branches
-    )
-    if f"beta/{curver}" not in branche_names:
-        (log.warning if (dryrun or force) else error)(
-            f"there's no 'beta/{curver}' branch in the worktree or in {remote}"
+    if len(remotes) > 1 and not remote:
+        (log.error if dryrun else error)(
+            f"multiple remotes defined: {', '.join(remotes)}"
         )
+    if remote and remote not in remotes:
+        error(f"requested {remote=} but found {', '.join(remotes)}")
+        (log.error if dryrun else error)(
+            f"user select {remote=} but only found {', '.join(remotes)}"
+        )
+    remote = remote or (remotes or [None]).pop()
+    log.debug("current remote '%s'", remote)
 
     # check we are on master
     current = repo.head.shorthand
     log.debug("current branch %s", current)
     if current != "master":
-        (log.warning if dryrun else error)(
-            f"current branch is [{current}] but must be master branch"
+        (log.error if dryrun else error)(
+            f"current branch is '{current}' but this script runs on the 'master' branch"
         )
 
     # check we have no uncommitted changes
@@ -56,15 +65,38 @@ def repo_checks(repo, error, dryrun, force, curver, newbranch):
 
     modified = {p for p, f in repo.status().items() if not ignore(f)}
     if modified:
-        (log.warning if (dryrun or force) else error)(
+        (log.error if (dryrun or force) else error)(
             "local modification staged for commit, use -f|--force to skip check"
         )
 
-    # check we haven't already branched
-    if newbranch in branche_names:
-        (log.warning if dryrun else error)(
-            f"new branch {newbranch} fund in current branche set"
+    # check the current version has a beta/<curver> branch
+    remote_branches = extract_beta_branches(repo.branches.remote, remote=remote)
+    local_branches = extract_beta_branches(repo.branches.local)
+
+    if not any(remote_branches | local_branches):
+        # no beta/X.Y.Z branches, we start fresh
+        return curver
+
+    is_in_local = bool([b for b in local_branches if b.endswith(f"beta/{curver}")])
+    is_in_remote = bool([b for b in remote_branches if b.endswith(f"beta/{curver}")])
+    if not (is_in_local or is_in_remote):
+        (log.error if (dryrun or force) else error)(
+            f"cannot find 'beta/{curver}' branch in the local or remote branches"
         )
+
+    newver = newversion(curver, mode)
+    is_in_local = bool([b for b in local_branches if b.endswith(f"beta/{newver}")])
+    is_in_remote = bool([b for b in remote_branches if b.endswith(f"beta/{newver}")])
+    if is_in_local:
+        (log.error if (dryrun or force) else error)(
+            f"found 'beta/{newver}' branch in the local branches"
+        )
+    if is_in_remote:
+        (log.error if (dryrun or force) else error)(
+            f"found 'beta/{newver}' branch in the remote branches"
+        )
+
+    return newver
 
 
 def parse_args(args=None):
@@ -82,6 +114,7 @@ def parse_args(args=None):
     parser.add_argument("-v", "--verbose", action="store_true")
     parser.add_argument("-f", "--force", action="store_true")
     parser.add_argument("-n", "--dry-run", dest="dryrun", action="store_true")
+    parser.add_argument("--remote", help="use remote")
     parser.add_argument("--no-checks", action="store_true")
 
     parser.add_argument(
@@ -98,13 +131,20 @@ def parse_args(args=None):
 
     options.checks = not options.no_checks
     options.error = parser.error
-    logging.basicConfig(level=logging.DEBUG if options.verbose else logging.INFO)
+
+    logging.basicConfig(
+        format="%(levelname)s:%(name)s:(dry-run) %(message)s"
+        if options.dryrun
+        else "%(levelname)s:%(name)s:%(message)s",
+        level=logging.DEBUG if options.verbose else logging.INFO,
+    )
+
     for d in ["verbose", "no_checks"]:
         delattr(options, d)
     return options.__dict__
 
 
-def run(mode, initfile, workdir, force, dryrun, error, checks):
+def run(mode, initfile, workdir, force, dryrun, error, checks, remote):
     workdir = workdir.resolve()
     log.debug("using working dir %s", workdir)
 
@@ -114,23 +154,31 @@ def run(mode, initfile, workdir, force, dryrun, error, checks):
         error(f"cannot find __version__ in {initfile}")
     log.info("current version '%s'", curver)
 
-    # generate the new version / branch name
-    newver = newversion(curver, mode)
-    newbranch = f"beta/{newver}"
-    log.info("creating new version %s -> %s (branch %s)", curver, newver, newbranch)
-
-    # repo
     repo = pygit2.Repository(workdir)
 
-    # various checks
-    if checks:
-        repo_checks(repo, error, dryrun, force, curver, newbranch)
+    # various checks and generate the new version / branch name
+    newver = repo_checks(repo, remote, error, dryrun, force, curver, mode)
+    newbranch = f"beta/{newver}"
+    if newver == curver:
+        log.info(
+            "creating first version branch '%s' (v. %s) from 'master'",
+            newbranch,
+            newver,
+        )
+    else:
+        log.info(
+            "creating new version branch '%s' (v. %s) from 'master' (%s)",
+            newbranch,
+            newver,
+            curver,
+        )
 
     # modify the __init__
-    log.info("updating init file %s%s", initfile, " (skip)" if dryrun else "")
+    log.info("updating init file %s (%s -> %s)", initfile, curver, newver)
     if not dryrun:
         tools.set_module_var(initfile, "__version__", newver)
 
+    # commit the updated __init__.py in the master branch
     msg = f"beta release {newver}"
     log.info("committing '%s'%s", msg, " (skip)" if dryrun else "")
     if not dryrun:
