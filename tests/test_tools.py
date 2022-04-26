5ac142b2f5d5a2b47e6e1770eabfc398390dba3d
antonio <a.cavallo@cavallinux.eu>
2022-04-26 04:29:33 -0400

    update tests

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -2,6 +2,7 @@ import pytest
 import itertools
 from setuptools_github import tools
 
+# these have the same format as generated from ${{ toJson(github) }}
 GIT_DUMPS = {
     "beta": {
         "ref": "refs/heads/beta/0.0.4",
@@ -21,23 +22,9 @@ GIT_DUMPS = {
 }
 
 
-def test_get_module_var(tmp_path):
-    with open(tmp_path / "in.txt", "w") as fp:
-        fp.write(
-            """
-# a test file
-A = 12
-B = 3+5
-C = "hello"
-# end of test
-"""
-        )
-    assert 12 == tools.get_module_var(tmp_path / "in.txt", "A")
-    assert "hello" == tools.get_module_var(tmp_path / "in.txt", "C")
-    pytest.raises(AssertionError, tools.get_module_var, tmp_path / "in.txt", "B")
-
-
 def test_hubversion():
+    "extracts from a GIT_DUMPS a (version, hash) tuple"
+
     fallbacks = [
         "123",
         "",
@@ -59,100 +46,121 @@ def test_hubversion():
         assert expected == tools.hubversion(gdata, fallback)
 
 
-def test_set_module_var(tmp_path):
-
-    with open(tmp_path / "in.txt", "w") as fp:
-        fp.write(
-            """
+def test_get_module_var(tmp_path):
+    "pulls variables from a file"
+    path = tmp_path / "in0.txt"
+    path.write_text(
+        """
 # a test file
-__version__ = ""
-__hash__ = "4.5.6"
-
-# end of test
-"""
-        )
-    version, txt = tools.set_module_var(tmp_path / "in.txt", "__version__", "6.7.8")
-    assert not version
-    assert (
-        txt
-        == """
-# a test file
-__version__ = "6.7.8"
-__hash__ = "4.5.6"
-
+A = 12
+B = 3+5
+C = "hello"
 # end of test
 """
     )
+    assert 12 == tools.get_module_var(path, "A")
+    assert "hello" == tools.get_module_var(path, "C")
+    pytest.raises(AssertionError, tools.get_module_var, path, "B")
 
-    with open(tmp_path / "in.txt", "w") as fp:
-        fp.write(
-            """
-# a test file
+
+def test_set_module_var_empty_file(tmp_path):
+    "check if the set_module_var will create a bew file"
+    path = tmp_path / "in1.txt"
+
+    path.write_text("# a fist comment line\n")
+    assert (
+        path.read_text().strip()
+        == """
+# a fist comment line
+""".strip()
+    )
+
+    tools.set_module_var(path, "__version__", "1.2.3")
+    tools.set_module_var(path, "__hash__", "4.5.6")
+    assert (
+        path.read_text().strip()
+        == """
+# a fist comment line
 __version__ = "1.2.3"
 __hash__ = "4.5.6"
+""".strip()
+    )
 
-# end of test
-"""
-        )
-    version, txt = tools.set_module_var(tmp_path / "in.txt", "__version__", "6.7.8")
-    assert version == "1.2.3"
-    assert (
-        txt
-        == """
-# a test file
-__version__ = "6.7.8"
+
+def test_set_module_var(tmp_path):
+    "handles set_module_var cases"
+    path = tmp_path / "in2.txt"
+
+    path.write_text(
+        """
+# a fist comment line
 __hash__ = "4.5.6"
-
 # end of test
 """
     )
-    version, txt = tools.set_module_var(tmp_path / "in.txt", "__hash__", "9.10.11")
+
+    version, txt = tools.set_module_var(path, "__version__", "1.2.3")
+    assert not version
+    assert (
+        txt.rstrip()
+        == """
+# a fist comment line
+__hash__ = "4.5.6"
+# end of test
+__version__ = "1.2.3"
+""".rstrip()
+    )
+
+    version, txt = tools.set_module_var(path, "__version__", "6.7.8")
+    assert version == "1.2.3"
+    assert (
+        txt.rstrip()
+        == """
+# a fist comment line
+__hash__ = "4.5.6"
+# end of test
+__version__ = "6.7.8"
+""".rstrip()
+    )
+
+    version, txt = tools.set_module_var(path, "__hash__", "9.10.11")
     assert version == "4.5.6"
     assert (
-        txt
+        txt.rstrip()
         == """
-# a test file
-__version__ = "6.7.8"
+# a fist comment line
 __hash__ = "9.10.11"
-
 # end of test
-"""
+__version__ = "6.7.8"
+""".rstrip()
     )
     return
 
-    tools.set_module_var(tmp_path / "in.txt", "__version__", "6.7.8")
-    tools.set_module_var(tmp_path / "in.txt", "__hash__", "8.9.10")
-
-    assert (
-        (tmp_path / "in.txt").read_text()
-        == """
-# a test file
-__version__ = "6.7.8"
-__hash__ = "8.9.10"
-
-# end of test
-"""
-    )
-
 
 def test_update_version(tmp_path):
-    def writeinit(initfile):
-        with open(initfile, "w") as fp:
-            fp.write(
-                """
+    "test the update_version processing"
+    from hashlib import sha224
+
+    def writeinit(path):
+        path.write_text(
+            """
 # a test file
 __version__ = "1.2.3"
 __hash__ = "4.5.6"
 
 # end of test
 """
-            )
+        )
+        return sha224(path.read_bytes()).hexdigest()
 
-    initfile = tmp_path / "in.txt"
-    writeinit(initfile)
+    initfile = tmp_path / "__init__.py"
+    hashval = writeinit(initfile)
 
+    # verify nothing has changed
     assert "1.2.3" == tools.update_version(initfile)
+    assert hashval == sha224(initfile.read_bytes()).hexdigest()
 
+    # we update the __version__/__hash__ from a master branch
     tools.update_version(initfile, GIT_DUMPS["master"])
     assert (
         initfile.read_text()
@@ -165,6 +173,7 @@ __hash__ = "2169f90c"
 """
     )
 
+    # we update __version__/__hash__ from a beta branch (note the b<build-number>)
     writeinit(initfile)
     tools.update_version(initfile, GIT_DUMPS["beta"])
     assert (
