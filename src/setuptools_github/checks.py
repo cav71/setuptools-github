70e49c02c60d15b1a9be37a61836dcd93614edf0
antonio <a.cavallo@cavallinux.eu>
2022-05-30 04:07:11 +0200

    split checks into a separate module

--- src/setuptools_github/checks.py
+++ src/setuptools_github/checks.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 from typing import List, Optional
 
 import pygit2  # type: ignore
+
+from . import tools
 
 
 class Check:
@@ -36,4 +39,26 @@ def check_has_single_remote(
         errors.append(
             Failure(f"requested remote={remote} but found {', '.join(remotes)}")
         )
+    return errors if _testmode else errors[:1]
+
+
+def check_initfile(initfile: Path, _testmode: bool = False) -> Optional[List[Check]]:
+    "check the initfile presence and contents"
+
+    errors = []
+    if _testmode or not initfile.exists():
+        errors.append(Failure(f"no file '{initfile}' found"))
+
+    curver = tools.get_module_var(initfile, "__version__", abort=False)
+    if _testmode or not curver:
+        errors.append(
+            Failure(
+                f"cannot find __version__ in {initfile}",
+                explain="""
+        The initfile should contain the __version__ module level variable;
+        it should be a text string in the MAJOR.MINOR.MICRO form.
+        """,
+            )
+        )
+
     return errors if _testmode else errors[:1]
--- tests/test_checks.py
+++ tests/test_checks.py
@@ -24,3 +24,29 @@ def test_check_has_single_remote(git_project_factory):
 
     # we can explicitly use a remote to refer to (this won't trigger an error)
     assert not sr.check_has_single_remote(project.repo, "origin/remote-name2")
+
+
+def test_check_initifile(git_project_factory):
+    "checks initfile presence and contents"
+
+    # we create a project without an initfile
+    project = git_project_factory("check-initfile").create(None)
+
+    # this will dump all issues at once
+    assert len(sr.check_initfile(project.initfile, _testmode=True)) == 2
+
+    # initfile not present -> fail
+    assert sr.check_initfile(project.initfile)
+    assert sr.check_initfile(project.initfile)[0].msg.startswith("no file")
+
+    project.initfile.parent.mkdir(parents=True, exist_ok=True)
+
+    # touching the file (but still failing because of the missing __version__ variable)
+    project.initfile.write_text("")
+    assert sr.check_initfile(project.initfile)[0].msg.startswith(
+        "cannot find __version__ in"
+    )
+
+    # we succeed on well formed initfile
+    project.initfile.write_text("__version__ = '1.2.3'")
+    assert not sr.check_initfile(project.initfile)
