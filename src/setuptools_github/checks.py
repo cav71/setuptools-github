5fbe267ccff25e7a016b0d0ee7d8b509f20a859e
antonio <a.cavallo@cavallinux.eu>
2022-06-03 11:33:51 +0200

    checks for remote

--- src/setuptools_github/checks.py
+++ src/setuptools_github/checks.py
@@ -62,3 +62,73 @@ def check_initfile(initfile: Path, _testmode: bool = False) -> Optional[List[Che
         )
 
     return errors if _testmode else errors[:1]
+
+
+def check_for_release(
+    repo: pygit2.Repository, initfile: Path, curver: str = "", _testmode: bool = False
+) -> Optional[List[Check]]:
+    from re import compile
+
+    regex_beta = compile(r"/?beta/(?P<ver>\d+([.]\d+)*)$")
+
+    errors = []
+
+    curver = curver or tools.get_module_var(initfile, "__version__", abort=False)
+    thisver = None
+    if regex_beta.match(repo.head.shorthand):
+        thisver = regex_beta.match(repo.head.shorthand).groupdict()["ver"]
+
+    # check we are in a beta branch
+    if _testmode or not regex_beta.match(repo.head.shorthand):
+        errors.append(
+            Failure(
+                f"release should start from 'beta/{curver}' "
+                f"branch (currently on {repo.head.shorthand})"
+            )
+        )
+
+    # check curver match the branch name
+    if _testmode or (thisver and thisver != curver):
+        errors.append(
+            Failure(
+                f"current branch 'beta/{curver}' doesn't match "
+                f"the initfile version {thisver}"
+            )
+        )
+
+    # check we aren't re-releasing
+    regex = compile("^refs/tags/")
+    tags = [r for r in repo.references if regex.match(r)]
+    if _testmode or (f"refs/tags/release/{curver}" in tags):
+        errors.append(Failure(f"tag release/{curver} present, cannot re-release"))
+
+    # check current branch is in sync with remote
+    # get all local and remote beta/ branches
+    # local_branches = {
+    #    b.rpartition("/")[2]: repo.lookup_branch(b)
+    #    for b in repo.branches.local
+    #    if regex_beta.search(b)
+    # }
+    remote_branches = {
+        b.rpartition("/")[2]: repo.lookup_branch(b, pygit2.GIT_BRANCH_REMOTE)
+        for b in repo.branches.remote
+        if regex_beta.search(b)
+    }
+
+    if _testmode or (
+        (curver in remote_branches)
+        and remote_branches[curver].target != repo.head.target
+    ):
+        target = remote_branches[curver].target if curver in remote_branches else None
+        errors.append(
+            Failure(
+                f"local and remote branches beta/{curver} are out of sync",
+                explain=f"""
+                The local branch {repo.head.shorthand} has
+                """
+                f"""different hash from remote ({repo.head.target} != {target})
+                """,
+            )
+        )
+
+    return errors if _testmode else errors[:1]
