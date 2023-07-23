3a59c989ded1b15c9ab9846d6f00f1cb031b80d8
antonio <a.cavallo@cavallinux.eu>
2023-07-23 14:53:35 +0200

    fix

--- src/setuptools_github/scm.py
+++ src/setuptools_github/scm.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 import dataclasses as dc
 import subprocess
-from typing import TypeAlias, Union
+from typing_extensions import TypeAlias
 from pathlib import Path
 
 
-ListOfArgs: TypeAlias = Union[str | Path | list[str | Path]]
+ListOfArgs: TypeAlias = str | Path | list[str | Path]
 
 
 @dc.dataclass
